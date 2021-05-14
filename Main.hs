70cc4a89a95a18534d3dd489d5d53068b702d992
Alex <alexander.korpas@protonmail.com>
2021-05-14 10:45:03 +0200

    first commit

---
+++ Main.hs
@@ -0,0 +1,16 @@
+import Route
+import RouteGUI
+import Graph  -- Create a module and use a sensible graph representation
+
+shortestPath :: Graph -> Name -> Name -> Maybe ([Name], Cost)
+shortestPath g from to = undefined -- TODO: implement Dijkstra's algorithm
+
+main :: IO ()
+main = undefined  -- TODO: read arguments, build graph, output shortest path
+
+startGUI :: IO ()
+startGUI = do
+  Right stops <- readStops "your-stops.txt"
+  Right lines <- readLines "your-lines.txt"
+  let graph = undefined -- TODO: build your graph here using stops and lines
+  runGUI stops lines graph shortestPath
